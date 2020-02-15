e23f938e9b7fe59e576a896447bb7c56292a2ace
Maxime Hacker <maximeh@google.com>
2020-02-15 22:43:42 +0100

    clang-format

--- remove_if.cc
+++ remove_if.cc
@@ -5,48 +5,46 @@
 void PrintVector();
 
 struct Data {
-    explicit Data(int val): val(val), old_val(val) {};
-    Data(const Data& other) = delete;
-    Data(Data&& other) {
-        if (val!=-1) {
-            std::cout << "Overwrite " << val << std::endl;
-        }
-        val = other.val;
-        other.val = -1;
-        std::cout << "Moved " << val << std::endl;
-        PrintVector();
+  explicit Data(int val) : val(val), old_val(val){};
+  Data(const Data& other) = delete;
+  Data(Data&& other) {
+    if (val != -1) {
+      std::cout << "Overwrite " << val << std::endl;
     }
-    Data& operator=(Data&& other) {
-        if (val!=-1) {
-            std::cout << "Overwrite " << val << std::endl;
-        }
-        val = other.val;
-        other.val = -1;
-        std::cout << "Moved " << val << std::endl;
-        PrintVector();
-        return *this;
+    val = other.val;
+    other.val = -1;
+    std::cout << "Moved " << val << std::endl;
+    PrintVector();
+  }
+  Data& operator=(Data&& other) {
+    if (val != -1) {
+      std::cout << "Overwrite " << val << std::endl;
     }
-    ~Data() {
-        std::cout << "Destructor called on " << old_val << std::endl;
-    }
+    val = other.val;
+    other.val = -1;
+    std::cout << "Moved " << val << std::endl;
+    PrintVector();
+    return *this;
+  }
+  ~Data() { std::cout << "Destructor called on " << old_val << std::endl; }
 
-    int val;
-    int old_val;
+  int val;
+  int old_val;
 };
-bool IsEven(const Data& d) { return d.val%2==0; }
+bool IsEven(const Data& d) { return d.val % 2 == 0; }
 
 static std::vector<Data>* vec;
 
-void PrintVector(){
-    for (const Data& d: *vec) std::cout << d.val <<  " ";
-    std::cout << std::endl;
+void PrintVector() {
+  for (const Data& d : *vec) std::cout << d.val << " ";
+  std::cout << std::endl;
 }
 
 int main() {
-    vec = new std::vector<Data>();
-    vec->reserve(20);
-    for (int i=0 ; i<20  ; i++) vec->emplace_back(i);
-    PrintVector();
-    vec->erase(std::remove_if(vec->begin(), vec->end(), IsEven), vec->end());
-    PrintVector();
+  vec = new std::vector<Data>();
+  vec->reserve(20);
+  for (int i = 0; i < 20; i++) vec->emplace_back(i);
+  PrintVector();
+  vec->erase(std::remove_if(vec->begin(), vec->end(), IsEven), vec->end());
+  PrintVector();
 }
